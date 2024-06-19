5a2f2e1eadaf0531b056710b536e3b018dfe80e9
Paul Wentzel <paul.wentzel@gmail.com>
2024-06-18 22:02:53 -0400

    tests passing

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,15 +1,21 @@
 
 function bubbleSort(arr) {
-
+  let swaps = 0;
     // Iterate through the array
-
+  for (let i = 0; i < arr.length - 1; i++) {
       // If the current value is greater than its neighbor to the right
         // Swap those values
+    if (arr[i] > arr[i + 1]) {
+      [arr[i + 1], arr[i]] = [arr[i], arr[i + 1]];
+      swaps++;
+      // Do not move this console.log
+      console.log(arr.join(","));
+    }
 
-        // Do not move this console.log
-        console.log(arr.join(","));
 
-    // If you get to the end of the array and no swaps have occurred, return
+  }
+  // If you get to the end of the array and no swaps have occurred, return
+  if (swaps > 0) bubbleSort(arr);
 
     // Otherwise, repeat from the beginning
 
